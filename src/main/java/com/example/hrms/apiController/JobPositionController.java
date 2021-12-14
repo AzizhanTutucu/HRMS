a14cfa564e30b031feff9c95a15baf1f3ba56752
AzizhanTutucu <aziztutucu99@gmail.com>
2021-12-14 19:05:48 +0300

    HRMS

---
+++ src/main/java/com/example/hrms/apiController/JobPositionController.java
@@ -0,0 +1,29 @@
+package com.example.hrms.apiController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.hrms.business.abstracts.JobPositionService;
+import com.example.hrms.entities.concretes.JobPosition;
+
+@RestController
+@RequestMapping("/api/jobs")
+public class JobPositionController {
+	private JobPositionService jobPositionService;
+
+	@Autowired
+	public JobPositionController(JobPositionService jobPositionService) {
+		super();
+		this.jobPositionService = jobPositionService;
+	}
+	@GetMapping("/getall")
+	
+	List<JobPosition> getAll(){
+		return this.jobPositionService.getAll();
+	}
+
+}
---
+++ src/main/java/com/example/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,46 @@
+package com.example.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "JobPosition")
+public class JobPosition {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="position_id")
+	private int id;
+	@Column(name="position_name")
+	private String positionName;
+	
+	public JobPosition() {
+		
+	}
+
+	public JobPosition(int id, String positionName) {
+		super();
+		this.id = id;
+		this.positionName = positionName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPositionName() {
+		return positionName;
+	}
+
+	public void setPositionName(String positionName) {
+		this.positionName = positionName;
+	}
+
+}
